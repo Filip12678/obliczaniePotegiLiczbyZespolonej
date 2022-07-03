b125d8d46a79cfac9f389e96f680b76e8c1d7753
Filip12678 <87238077+Filip12678@users.noreply.github.com>
2022-07-03 20:28:07 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,37 @@
+var wynik = [];
+
+function potegowanieLiczbZespolonych(a, b, c, d) {
+	
+	var e = Math.E;
+	var kat = Math.acos( (a / Math.sqrt((a*a + b*b))) );
+	
+	var czescRzeczywista = Math.pow( (a*a + b*b), (c/2) ) * Math.pow(e, (-1 * d * kat)) * Math.cos( (c * kat + 0.5 * d * Math.log( (a*a + b*b) )));
+	
+	var czescZespolona = Math.pow( (a*a + b*b), (c/2) ) * Math.pow(e, (-1 * d * kat)) * Math.sin( (c * kat + 0.5 * d * Math.log( (a*a + b*b) )));
+	
+	console.log(czescRzeczywista, czescZespolona);
+	
+	wynik.push(czescRzeczywista);
+	wynik.push(czescZespolona);
+	
+	
+}
+
+
+
+function myFunction() {
+	
+	var a = parseFloat(document.getElementById('a').value);
+	var b = parseFloat(document.getElementById('b').value);
+	var c = parseFloat(document.getElementById('c').value);
+	var d = parseFloat(document.getElementById('d').value);
+	
+	
+	potegowanieLiczbZespolonych(a, b, c, d);
+	
+	
+	
+document.getElementById("wynik").innerHTML = `Część rzeczywista: ${wynik[0]}<br>Część zespolona: ${wynik[1]}i`;
+	
+	
+}
